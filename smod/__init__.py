624f32d3a9cadd11244979238389152bec24b25f
Eugen Wintersberger <eugen.wintersberger@desy.de>
2011-12-20 11:04:47 +0100

    Added Python modules for scons from libpniutils to libpninx.

---
+++ smod/__init__.py
@@ -0,0 +1,6 @@
+from version import ProgramVersion
+from version import GCCVersionParser
+from version import DoxyVersionParser
+from program import CheckProgram
+from changelog import DebChangelogFile
+
