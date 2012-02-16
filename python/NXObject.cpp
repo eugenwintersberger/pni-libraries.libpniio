30683e7851e707d6ab163aba08ff770a42a5f1c9
Eugen Wintersberger <eugen.wintersberger@desy.de>
2012-02-16 13:37:37 +0100

    NXObject wrapper compiles again.

--- python/NXObject.cpp
+++ python/NXObject.cpp
@@ -15,29 +15,26 @@
 #include <pni/utils/ScalarObject.hpp>
 
 #include "../src/NXObject.hpp"
-#include "../src/h5/NXObjectH5Implementation.hpp"
-#include "../src/h5/NXGroupH5Implementation.hpp"
-#include "../src/h5/NXFileH5Implementation.hpp"
-#include "../src/h5/NXFieldH5Implementation.hpp"
-#include "../src/h5/NXNumericFieldH5Implementation.hpp"
-#include "../src/h5/NXStringFieldH5Implementation.hpp"
-#include "../src/h5/NXBinaryFieldH5Implementation.hpp"
+
+#include "../src/h5/H5AttributeObject.hpp"
+#include "../src/h5/H5Dataset.hpp"
+#include "../src/h5/H5Group.hpp"
+#include "../src/h5/H5File.hpp"
+
 
 #include "NXObject.hpp"
 
 using namespace pni::utils;
 using namespace pni::nx::h5;
+using namespace pni::nx;
 using namespace boost::python;
 
 void wrap_nxobject(){
-    NXOBJECT_WRAPPER(NXObject_NXObject,NXObjectH5Implementation);
+    NXOBJECT_WRAPPER(NXObject_NXObject,H5AttributeObject);
 	//===================Wrapping NumericObject=================================
-    NXOBJECT_WRAPPER(NXObject_NXGroup,NXGroupH5Implementation);
-    NXOBJECT_WRAPPER(NXObject_NXField,NXFieldH5Implementation);
-    NXOBJECT_WRAPPER(NXObject_NXNumericField,NXNumericFieldH5Implementation);
-    NXOBJECT_WRAPPER(NXObject_NXStringField,NXStringFieldH5Implementation);
-    NXOBJECT_WRAPPER(NXObject_NXBinaryField,NXBinaryFieldH5Implementation);
-    NXOBJECT_WRAPPERNOCOP(NXObject_NXFile,NXFileH5Implementation);
+    NXOBJECT_WRAPPER(NXObject_NXGroup,H5Group);
+    NXOBJECT_WRAPPER(NXObject_NXField,H5Dataset);
+    NXOBJECT_WRAPPERNOCOP(NXObject_NXFile,H5File);
 }
 
 
