e4946e942fb29cbcef614c92bfb79a7a87dde189
Eugen Wintersberger <eugen.wintersberger@desy.de>
2012-04-19 11:29:01 +0200

    Added a new example using threads. Actually this throws errors at runtime but despite from this its operating correctly.

---
+++ doc/examples/c++/thread_ex1.cpp
@@ -0,0 +1,56 @@
+/*example for the library using a thread*/
+
+#include<thread>
+#include<iostream>
+#include<pni/hdf5/hdf5.h>
+
+#include<pni/utils/Types.hpp>
+#include<pni/nx/NX.hpp>
+
+using namespace pni::utils;
+using namespace pni::nx::h5;
+
+
+class Writer{
+    private:
+        String _filename;
+    public:
+        Writer(const String &fname)
+        {
+            _filename = fname;
+        }
+
+        void operator()(){
+            std::cout<<"create file"<<std::endl;
+            //works with HDF5
+            /*
+            hid_t h5f =
+                H5Fcreate(_filename.c_str(),H5F_ACC_TRUNC,H5P_DEFAULT,H5P_DEFAULT);
+
+            H5Fclose(h5f);
+            */
+            NXFile f = NXFile::create_file(_filename,true);
+            auto g = f.create_group("log");
+            auto d = g.create_field<String>("data");
+            d.write("Staring new log");
+
+            std::cout<<"close file"<<std::endl;
+            f.close();
+        }
+};
+
+
+int main(int argc,char **argv)
+{
+    Writer writer("userlog.nx");
+    std::thread wthread(writer);
+
+
+
+
+
+    //in the very end we need to join the thread
+    wthread.join();
+
+    return 0;
+}    
