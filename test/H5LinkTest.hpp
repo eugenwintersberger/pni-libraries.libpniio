9797766f9a1d8b29389c5570db43c10ec3e3b157
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2013-02-02 11:58:12 +0100

    Added licence headers to HDF5 backend tests.

--- test/H5LinkTest.hpp
+++ test/H5LinkTest.hpp
@@ -1,3 +1,25 @@
+/*
+ * (c) Copyright 2012 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
+ *
+ * This file is part of libpniio.
+ *
+ * libpniio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * libpniio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libpniio.  If not, see <http://www.gnu.org/licenses/>.
+ *************************************************************************
+ *
+ * Created on: Feb 10, 2012
+ *     Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+ */
 #pragma once
 
 
@@ -7,13 +29,13 @@
 #include <pni/core/types.hpp>
 
 
-#include "../src/h5/H5Link.hpp"
-#include "../src/h5/H5File.hpp"
-#include "../src/h5/H5Group.hpp"
-#include "../src/h5/H5Dataspace.hpp"
-#include "../src/h5/H5Datatype.hpp"
-#include "../src/h5/H5DatatypeFactory.hpp"
-#include "../src/h5/H5Dataset.hpp"
+#include <pni/io/nx/h5/H5Link.hpp>
+#include <pni/io/nx/h5/H5File.hpp>
+#include <pni/io/nx/h5/H5Group.hpp>
+#include <pni/io/nx/h5/H5Dataspace.hpp>
+#include <pni/io/nx/h5/H5Datatype.hpp>
+#include <pni/io/nx/h5/H5DatatypeFactory.hpp>
+#include <pni/io/nx/h5/H5Dataset.hpp>
 
 using namespace pni::core;
 using namespace pni::io::nx::h5;
