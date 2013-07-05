be322ed928f6c9d86a700d01e9f1ea7adecca47c
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2013-07-05 12:27:55 +0200

    Added get_size visitor.

--- src/nx/nxvariant.hpp
+++ src/nx/nxvariant.hpp
@@ -40,6 +40,7 @@
 #include "variant_algo/get_shape.hpp"
 #include "variant_algo/get_type.hpp"
 #include "variant_algo/get_rank.hpp"
+#include "variant_algo/get_size.hpp"
 #include "variant_algo/as_field.hpp"
 #include "variant_algo/as_group.hpp"
 #include "variant_algo/as_attribute.hpp"
