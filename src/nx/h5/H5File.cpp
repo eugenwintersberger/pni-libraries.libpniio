addd624da7de1fde019268aaa7bf2978df765582
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2014-04-01 11:53:54 +0200

    Added garbage collector.
    Update issue 54
    State: WontFix
    Labels: Priority-Low
    This bug is related to the HDf5 library. We cannot fix this thus the status of
    the bug has been reset to WontFix. As the bug becomes apparent only in cases
    where the uses makes tremendously stupid choices in the chunk size the priority
    will be reduced to Low.

--- src/nx/h5/H5File.cpp
+++ src/nx/h5/H5File.cpp
@@ -57,7 +57,6 @@ namespace h5{
     {
         if(is_valid())
         {
-            /*
             std::cerr<<"File: "<<name()<<std::endl;
             std::cerr<<"Open files:      "<<
                 H5Fget_obj_count(id(),H5F_OBJ_FILE)<<std::endl;
@@ -69,10 +68,10 @@ namespace h5{
                 H5Fget_obj_count(id(),H5F_OBJ_DATATYPE)<<std::endl;
             std::cerr<<"Open attributes: "<<
                 H5Fget_obj_count(id(),H5F_OBJ_ATTR)<<std::endl;
-            */
             H5Fflush(id(),H5F_SCOPE_GLOBAL);
             H5Fclose(id());
             H5Object::reset_id();
+            H5garbage_collect();
         }
     }
 
@@ -100,7 +99,6 @@ namespace h5{
         //check for open objects in the file
         if(is_valid())
         {
-            /*
             std::cerr<<"File: "<<name()<<std::endl;
             std::cerr<<"Open files:      "<<
                 H5Fget_obj_count(id(),H5F_OBJ_FILE)<<std::endl;
@@ -112,10 +110,10 @@ namespace h5{
                 H5Fget_obj_count(id(),H5F_OBJ_DATATYPE)<<std::endl;
             std::cerr<<"Open attributes: "<<
                 H5Fget_obj_count(id(),H5F_OBJ_ATTR)<<std::endl;
-            */
             H5Fflush(id(),H5F_SCOPE_GLOBAL);
             H5Fclose(id());
             H5Object::reset_id();
+            H5garbage_collect();
         }
 
     }
