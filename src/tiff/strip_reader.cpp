7d911d829f428ad3f0cf20e842a6370f789bc61c
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2014-06-02 13:36:46 +0200

    Fixed header comment style
    Fixed the comment style in the license headers of the implemenation files.

--- src/tiff/strip_reader.cpp
+++ src/tiff/strip_reader.cpp
@@ -1,30 +1,26 @@
-/*
- * Implementation of class TIFFStripReader
- *
- * (c) Copyright 2011 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
- *
- * This file is part of libpniio.
- *
- * libpniio is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * libpniio is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with libpniio.  If not, see <http://www.gnu.org/licenses/>.
- *************************************************************************
- *
- * Implementation of class TIFFStripReader.
- *
- * Created on: Jun 22, 2011
- *     Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
- *
- */
+//
+// (c) Copyright 2011 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
+//
+// This file is part of libpniio.
+//
+// libpniio is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// libpniio is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with libpniio.  If not, see <http://www.gnu.org/licenses/>.
+// ===========================================================================
+//
+// Created on: Jun 22, 2011
+//     Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+//
+//
 
 #include <pni/core/error.hpp>
 #include <pni/io/tiff/strip_reader.hpp>
