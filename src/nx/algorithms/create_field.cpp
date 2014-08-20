8e4756c1c92063d3ba3de71e6f16b4154ccc6a64
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2014-08-20 08:48:24 +0200

    Added algorithms directory in sources

---
+++ src/nx/algorithms/create_field.cpp
@@ -0,0 +1,43 @@
+//
+// (c) Copyright 2013 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
+//
+// This file is part of libpniio.
+//
+// libpniio is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// libpniio is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with libpniio.  If not, see <http://www.gnu.org/licenses/>.
+// ===========================================================================
+//
+// Created on: Jul 4, 2013
+//     Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+//
+
+#include <pni/io/nx/algorithms/create_field.hpp>
+
+namespace pni{
+namespace io{
+namespace nx{
+
+    nxpath get_path(const nxpath &p)
+    {
+        return p;
+    }
+
+    nxpath get_path(const string &p)
+    {
+        return nxpath::from_string(p);
+    }
+
+//end of namespace
+}
+}
+}
