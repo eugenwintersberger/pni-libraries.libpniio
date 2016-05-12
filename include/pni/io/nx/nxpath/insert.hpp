6e7851f63a54e2a91c3cf54b54c7c6c8e1cfea66
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2016-05-12 13:29:39 +0200

    Fixed some issues concerining the boost libraries
    * Solved implicit conversion problem for boost::optional in
      src/nx/xml/node.cpp
    * solved issue with xml_writter_settings in /src/nx/xml/node.cpp
      The original template argument char did not work. Use now the key type of the
      Ptree structure.
    * Fixed header problem in insert.hpp
      The original header file is no longer available in newer versions of
      boost::spirit.

--- include/pni/io/nx/nxpath/insert.hpp
+++ include/pni/io/nx/nxpath/insert.hpp
@@ -22,7 +22,7 @@
 //
 #pragma once
 
-#include <boost/spirit/home/phoenix/function/function.hpp>
+#include <boost/spirit/include/phoenix.hpp>
 
 namespace pni{
 namespace io{
--- src/nx/xml/node.cpp
+++ src/nx/xml/node.cpp
@@ -122,13 +122,14 @@ namespace xml{
     {
         auto attr = parent.get_child_optional(attribute_path(name));
 
-        return attr;
+        return attr.is_initialized();
     }
 
     //------------------------------------------------------------------------
     std::ostream &operator<<(std::ostream &o,const node &n)
     {
-        boost::property_tree::xml_writer_settings<char> settings('\t',1);
+        using key_type = typename node::key_type;
+        boost::property_tree::xml_writer_settings<key_type> settings('\t',1);
 
         boost::property_tree::write_xml(o,n,settings);
         return o;
