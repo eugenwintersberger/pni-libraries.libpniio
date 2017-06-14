ee21ed5a30caca2f315977c824a7e78a486a9eec
Eugen Wintersberger <eugen.wintersberger@desy.de>
2017-06-14 15:23:28 +0200

    Added CMake package files

---
+++ pniio-config.cmake
@@ -0,0 +1,19 @@
+# if the required Boost targets do not exist we have to add them
+if((NOT TARGET pnicore_shared))
+
+    find_package(pnicore REQUIRED)
+    #
+    # this will also pull in Boost::system and Boost::program_options
+    #
+endif()
+
+if((NOT TARGET Boost::filesystem) OR (NOT TARGET Boost::regex))
+
+	find_package(Boost 1.41 REQUIRED COMPONENTS filesystem regex)
+	if(NOT Boost_FOUND)
+		message(FATAL_ERROR "Could not find boost libraries - you may have to set the BOOST_ROOT variable!")
+	endif()
+endif()
+
+link_directories(${Boost_LIBRARY_DIRS})
+include(${CMAKE_CURRENT_LIST_DIR}/pniio_targets.cmake)
