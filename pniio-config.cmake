9246f6842aa45b5b81a5c985797d92aceed93c33
Eugen Wintersberger <eugen.wintersberger@desy.de>
2017-06-15 11:10:59 +0200

    I guess now dependencies should work ;)

--- pniio-config.cmake
+++ pniio-config.cmake
@@ -7,21 +7,22 @@ if((NOT TARGET pnicore_shared))
     #
 endif()
 
-set(BOOST_REQUIRED_COMPONENTS)
+set(BOOST_COMPONENTS)
 
 if(NOT TARGET Boost::filesystem)
-    list(APPEND BOOST_REQUIRED_COMPONENTS "Boost::filesystem")
+    list(APPEND BOOST_COMPONENTS filesystem)
 endif()
 
 if(NOT TARGET Boost::regex)
-    list(APPEND BOOST_REQUIRED_COMPONENTS "Boost::regex")
+    list(APPEND BOOST_COMPONENTS regex)
 endif()
 
 if(NOT TARGET Boost::date_time)
-    list(APPEND BOOST_REQUIRED_COMPONENTS "Boost::date_time")
+    list(APPEND BOOST_COMPONENTS date_time)
 endif()
 
-find_package(Boost 1.41 REQUIRED COMPONENTS ${BOOST_REQUIRED_COMPONENTS})
+message(STATUS "Required Boost components: ${BOOST_COMPONENTS}")
+find_package(Boost 1.41 REQUIRED COMPONENTS ${BOOST_COMPONENTS})
 if(NOT Boost_FOUND)
 	message(FATAL_ERROR "Could not find boost libraries - you may have to set the BOOST_ROOT variable!")
 endif()
