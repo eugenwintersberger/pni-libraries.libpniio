837d28d95a4e75e78667c1188d57a886b2d04f1d
Eugen Wintersberger <eugen.wintersberger@desy.de>
2017-06-15 10:21:51 +0200

    Added missing Boost::date_time dependency to the package

--- pniio-config.cmake
+++ pniio-config.cmake
@@ -7,9 +7,9 @@ if((NOT TARGET pnicore_shared))
     #
 endif()
 
-if((NOT TARGET Boost::filesystem) OR (NOT TARGET Boost::regex))
+if((NOT TARGET Boost::filesystem) OR (NOT TARGET Boost::regex) or (NOT TARGET Boost::date_time))
 
-	find_package(Boost 1.41 REQUIRED COMPONENTS filesystem regex)
+	find_package(Boost 1.41 REQUIRED COMPONENTS filesystem regex date_time)
 	if(NOT Boost_FOUND)
 		message(FATAL_ERROR "Could not find boost libraries - you may have to set the BOOST_ROOT variable!")
 	endif()
