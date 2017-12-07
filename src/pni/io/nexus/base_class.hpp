06386873b5aecd4b45429e13638d5114f48faff2
Eugen Wintersberger <eugen.wintersberger@desy.de>
2017-12-07 15:32:27 +0100

    Implemented simple BaseClass interface
    Update #43

---
+++ src/pni/io/nexus/base_class.hpp
@@ -0,0 +1,58 @@
+//
+// (c) Copyright 2017 DESY
+//
+// This file is part of libpniio.
+//
+// libpniio is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// libpniio is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with libpniio.  If not, see <http://www.gnu.org/licenses/>.
+// ===========================================================================
+//
+// Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+// Created on: Dec 7, 2017
+//
+#pragma once
+
+#include <h5cpp/hdf5.hpp>
+
+namespace pni {
+namespace io {
+namespace nexus {
+
+//!
+//! @brief representation of a NeXus base class
+//!
+//! A NeXus base class is in fact an HDF5 group with an NX_class attribute
+//! attached to it which tells the user what particular type of group he or
+//! she is dealing with.
+//!
+//! This is a very thing utility class which provides the full interface of a
+//! HDF5 group. But requires a value for NX_class to be set.
+//!
+class BaseClass : public hdf5::node::Group
+{
+  public:
+    BaseClass() = default;
+    BaseClass(const hdf5::node::Node &node);
+    BaseClass(const hdf5::node::Group &parent,
+              const hdf5::Path &path,
+              const std::string &class_name,
+              const hdf5::property::LinkCreationList &lcpl = hdf5::property::LinkCreationList(),
+              const hdf5::property::GroupCreationList &gcpl = hdf5::property::GroupCreationList(),
+              const hdf5::property::GroupAccessList &gapl = hdf5::property::GroupAccessList());
+
+    std::string get_class() const;
+};
+
+} // namespace nexus
+} // namespace io
+} // namespace pni
