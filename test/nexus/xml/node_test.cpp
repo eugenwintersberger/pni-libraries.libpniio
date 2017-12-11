0dcb40a41019448fdc7f0abf7633ece2f473d8be
Eugen Wintersberger <eugen.wintersberger@desy.de>
2017-12-11 14:20:52 +0100

    Added XML node tests
    Update #52

---
+++ test/nexus/xml/node_test.cpp
@@ -0,0 +1,108 @@
+//
+// (c) Copyright 2017 DESY
+//
+// This file is part of libpniio.
+//
+// libpniio is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// libpniio is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with libpniio.  If not, see <http://www.gnu.org/licenses/>.
+// ===========================================================================
+//
+// Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+// Created on: Dec 11, 2017
+//
+#include <boost/test/unit_test.hpp>
+#include <pni/io/nexus/xml/node.hpp>
+#include <pni/io/exceptions.hpp>
+#include <pni/core/types.hpp>
+#include <pni/core/error.hpp>
+
+using namespace pni::core;
+using namespace pni::io::nexus;
+
+static const string node_from_string_str =
+                                    "<node>\n<group> </group>\n<group>"
+                                    " </group>\n<group> </group>\n </node>";
+static const string node_from_bad_str = "this has to fail";
+
+struct InqueryFixture
+{
+    xml::Node group;
+
+    InqueryFixture():
+        group(xml::create_from_file("node/inquery.xml").get_child("group"))
+    {}
+
+};
+
+BOOST_AUTO_TEST_SUITE(NexusXMLTest)
+BOOST_AUTO_TEST_SUITE(XMLNodeTest)
+
+BOOST_AUTO_TEST_SUITE(CreationTest)
+
+//-------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(test_from_file)
+{
+  xml::Node n = xml::create_from_file("node/node_from_str.xml");
+  BOOST_CHECK(!n.empty());
+  BOOST_CHECK_EQUAL(n.size(),1);
+
+  //has to fail because file does not exist
+  BOOST_CHECK_THROW(xml::create_from_file("bla.xml"), pni::core::file_error);
+
+  //not a well formed XML file
+  BOOST_CHECK_THROW(xml::create_from_file("node/node_from_bad_file.xml"),
+                    pni::io::parser_error);
+
+
+}
+
+//-------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(test_from_string)
+{
+  xml::Node n = xml::create_from_string(node_from_string_str);
+
+  BOOST_CHECK(!n.empty());
+  BOOST_CHECK_EQUAL(n.size(),1);
+
+  BOOST_CHECK_THROW(xml::create_from_string(node_from_bad_str),
+                    pni::io::parser_error);
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
+BOOST_FIXTURE_TEST_SUITE(InqueryTest,InqueryFixture)
+
+
+//-------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(test_get_attribute)
+{
+
+  BOOST_CHECK_NO_THROW(xml::get_attribute(group,"name"));
+
+  //not a well formed XML file
+  BOOST_CHECK_THROW(xml::get_attribute(group,"type"), key_error);
+}
+
+//-------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE(test_has_attribute)
+{
+
+  BOOST_CHECK(xml::has_attribute(group,"name"));
+  BOOST_CHECK(!xml::has_attribute(group,"type"));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
