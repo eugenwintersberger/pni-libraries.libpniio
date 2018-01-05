462f9a35902bff0f029d3e729d3584ffd57ef742
Eugen Wintersberger <eugen.wintersberger@desy.de>
2018-01-05 09:04:51 +0100

    Added more example code to the threaded stuff
    Update #27

---
+++ doc/examples/nexus/thread_writer.hpp
@@ -0,0 +1,56 @@
+//
+// (c) Copyright 2018 DESY
+//
+// This file is part of libpniio.
+//
+// libpniio is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// libpniio is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with libpniio.  If not, see <http://www.gnu.org/licenses/>.
+// ===========================================================================
+//
+// Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+// Created on: Jan 5, 2018
+//
+#pragma once
+
+#include <h5cpp/hdf5.hpp>
+#include <boost/filesystem.hpp>
+#include "thread_ex1.hpp"
+
+class Writer
+{
+  private:
+    boost::filesystem::path _filename;
+    StringIOQueue          &_queue;
+    hdf5::file::File        _file;
+    hdf5::node::Dataset     _data;
+    hdf5::node::Dataset     _time;
+    hdf5::dataspace::Hyperslab _selection;
+    size_t _index;
+
+    //private method writing a single log entry
+    void _write_entry(const std::string &s);
+
+    //method creating the initial structure of the log
+    void _init_log();
+  public:
+    Writer(const boost::filesystem::path &fname,StringIOQueue &q):
+      _filename(fname),
+      _queue(q),
+      _file(),
+      _data(),
+      _selection(),
+      _index(0)
+    { }
+
+    void operator()();
+};
