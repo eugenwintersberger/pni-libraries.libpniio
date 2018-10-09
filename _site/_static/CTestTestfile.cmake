eff0965dd915ebc00fc662199a7a7e35f11f8cc0
Jan Kotanski <jankotan@gmail.com>
2018-10-09 11:17:50 +0200

    docu 1.2.5

--- _site/_static/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jkotan/sources/libpniio/doc/_static
-# Build directory: /home/jkotan/sources/libpniio-build/doc/_static
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
