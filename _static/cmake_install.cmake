0c05c3758aa2b89b159f59d4353d5cfe95c2601c
Jan Kotanski <jankotan@gmail.com>
2021-02-15 17:51:33 +0100

    update docu

--- _static/cmake_install.cmake
+++ _static/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/home/jkotan/pnitest")
+  set(CMAKE_INSTALL_PREFIX "/opt/pni")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
