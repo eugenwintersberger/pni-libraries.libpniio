69b315074f14c7129bdf2b7e4439aaab6b8b2556
Jan Kotanski <jankotan@gmail.com>
2021-08-04 18:19:01 +0200

    update documentation

--- _static/cmake_install.cmake
+++ _static/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/opt/pninexus")
+  set(CMAKE_INSTALL_PREFIX "/home/jkotan/pninexustest")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
