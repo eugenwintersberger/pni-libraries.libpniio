756e955f80b766c1098eaf490b2a18e625ffe0b6
jkotan <jan.kotanski@desy.de>
2021-11-25 18:41:14 +0000

    Build: (d3a3649) update documentation

--- _sources/api/ascii.txt
+++
@@ -1,9 +0,0 @@
-============
-ASCII IO API
-============
-
-
-.. toctree::
-   
-   ascii_parsers
-   ascii_spreadsheet
--- _static/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jkotan/sources/libpninexus/doc/_static
-# Build directory: /home/jkotan/sources/libpninexus-build/doc/_static
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
