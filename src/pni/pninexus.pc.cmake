48c7ea264f9eb07883fe1344bc0391683f84245f
Jan Kotanski <jankotan@gmail.com>
2022-01-27 14:31:06 +0100

    change h5cpp to h5cpp::h5cpp

--- src/pni/pninexus.pc.cmake
+++ src/pni/pninexus.pc.cmake
@@ -7,6 +7,6 @@ Name: @CMAKE_PROJECT_NAME@
 Description: PNI NeXus library
 Version: @LIBRARY_VERSION@
 Cflags: -I${includedir} -I@HDF5_INCLUDE_DIRS@
-Requires: h5cpp
+Requires: h5cpp::h5cpp
 Libs: -L${libdir} -lpninexus
 Libs.private:  -L@HDF5_LIBRARY_DIRS@ -lhdf5 -lhdf5_hl -lz -lboost_filesystem
