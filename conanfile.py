e28b869058347321183a361d54fb4b62d83e7982
Your Name <jankotan@gmail.com>
2022-02-03 10:07:53 +0100

    change boost to 1.77

--- conanfile.py
+++ conanfile.py
@@ -33,9 +33,9 @@ class H5CppConan(ConanFile):
 
     def requirements(self):
         # self.requires("hdf5/1.12.0")
-        # self.requires("boost/1.77.0")
+        self.requires("boost/1.77.0")
         self.requires("hdf5/1.10.6")
-        self.requires("boost/1.69.0")
+        # self.requires("boost/1.69.0")
         self.requires("doxygen/1.9.2")
         # self.requires("zlib/1.2.11")
         # self.requires("bzip2/1.0.8")
