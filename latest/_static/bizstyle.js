56ef839b02f4f2f66a05cfacfdeb123fd24775d7
jkotan <jan.kotanski@desy.de>
2022-05-05 13:59:15 +0000

    Build: (b169f96) Build: (1225471) Build: (6cde7aa) Build: (55351c5) Build: (4233ac6) Build: (045066c) Build: (eb647b6) Build: (e2c5480) Build: (6f57bb0) Build: (97fc12e) Build: (0f5e8d6) Build: (bc1003a) Build: (249e621) Build: (07f7894) Build: (ee17d0d) Build: (c953785) Build: (4eb2447) remove doc/api

--- latest/_static/bizstyle.js
+++ latest/_static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("libpninexus (latest) documentation");
+        $("li.nav-item-0 a").text("libpninexus 3.0.0 documentation");
     }
 });
