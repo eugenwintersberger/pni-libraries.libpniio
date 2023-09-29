ed0fb26f4b44f0fc9f9f6ba095efdbbc77f3f9cf
Jan Kotanski <jankotan@gmail.com>
2023-09-29 11:46:13 +0200

    add printouts

--- test/nexus/xml/create_test.cpp
+++ test/nexus/xml/create_test.cpp
@@ -196,22 +196,30 @@ BOOST_AUTO_TEST_CASE(add_filters)
   using hdf5::node::get_node;
   using hdf5::node::Type;
   fs::path file = "create/filters.xml";
+  std::cout << "AF0" << std::endl;
   BOOST_CHECK_NO_THROW(xml::create_from_file(root_group,file));
+  std::cout << "AF1" << std::endl;
 
   BOOST_CHECK(get_node(root_group,"/scan").type() == Type::Group);
   BOOST_CHECK(get_node(root_group,"/scan/data").type() == Type::Group);
   BOOST_CHECK(get_node(root_group,"/scan/data/pilatus").type() == Type::Dataset);
   hdf5::node::Dataset dataset = root_group.get_dataset("/scan/data/pilatus");
+  std::cout << "AF2" << std::endl;
   auto dcpl = dataset.creation_list();
+  std::cout << "AF3" << std::endl;
 
   hdf5::filter::ExternalFilters filters;
   auto flags = filters.fill(dcpl);
+  std::cout << "AF4: " << dcpl.nfilters() << " " << filters.size() << std::endl;
   BOOST_CHECK(dcpl.nfilters() == 6);
   BOOST_CHECK(flags.size() == 6);
   BOOST_CHECK(filters.size() == 6);
 
   BOOST_CHECK(filters[0].id() == H5Z_FILTER_SHUFFLE);
 
+  for(auto a: filters[0].cd_values()){
+    std::cout <<"SHo: "<< a << std::endl;
+  }
   BOOST_CHECK(filters[0].cd_values().size() == 1ul);  // ??
   BOOST_CHECK(filters[0].cd_values()[0] == 2ul);      // ??
   BOOST_CHECK(flags[0] == hdf5::filter::Availability::Optional);
@@ -220,6 +228,9 @@ BOOST_AUTO_TEST_CASE(add_filters)
 
   std::vector<unsigned int> rate {4u};
   BOOST_REQUIRE_EQUAL(filters[1].cd_values().size(), rate.size());
+  for(auto a: filters[1].cd_values()){
+    std::cout <<"DEo: "<< a << std::endl;
+  }
   for(size_t i = 0; i < rate.size(); ++i){
     BOOST_CHECK_EQUAL(filters[1].cd_values()[i], rate[i]);
   }
@@ -229,6 +240,9 @@ BOOST_AUTO_TEST_CASE(add_filters)
 
   std::vector<unsigned int> nbit_opts {8u, 0u, 0u, 0u, 0u, 0u, 0u, 0u};
   BOOST_REQUIRE_EQUAL(filters[2].cd_values().size(), nbit_opts.size());
+  for(auto a: filters[2].cd_values()){
+    std::cout <<"NBo: "<< a << std::endl;
+  }
   for(size_t i = 0; i < nbit_opts.size(); ++i){
     BOOST_CHECK_EQUAL(filters[2].cd_values()[i], nbit_opts[i]);
   }
@@ -238,29 +252,94 @@ BOOST_AUTO_TEST_CASE(add_filters)
 
   std::vector<unsigned int> szip_opts {137u, 16u, 16u, 512u}; // ??
   BOOST_REQUIRE_EQUAL(filters[3].cd_values().size(), szip_opts.size());
+  for(auto a: filters[3].cd_values()){
+    std::cout <<"SZo: "<< a << std::endl;
+  }
   for(size_t i = 0; i < szip_opts.size(); ++i){
     BOOST_CHECK_EQUAL(filters[3].cd_values()[i], szip_opts[i]);
   }
   BOOST_CHECK(flags[3] == hdf5::filter::Availability::Optional);
 
+  std::cout << "AF5 F4 : " << filters[4].id() << std::endl;
   BOOST_CHECK(filters[4].id() == 32008);
 
   std::vector<unsigned int> bs_opts {0u, 3u, 2u, 0u, 0u};    // ??
   BOOST_REQUIRE_EQUAL(filters[4].cd_values().size(), bs_opts.size());
-  // for(size_t i = 0; i < bs_opts.size(); ++i){
-  //   BOOST_CHECK_EQUAL(filters[4].cd_values()[i], bs_opts[i]);
-  // }
-  //BOOST_CHECK(flags[4] == hdf5::filter::Availability::Mandatory);
+  for(auto a: filters[4].cd_values()){
+    std::cout <<"BSo: "<< a << std::endl;
+  }
+  for(size_t i = 1; i < 3; ++i){
+    BOOST_CHECK_EQUAL(filters[4].cd_values()[i], bs_opts[i]);
+  }
   BOOST_CHECK(flags[4] == hdf5::filter::Availability::Optional);
 
+  std::cout << "AF5 F5 : " << filters[5].id() << std::endl;
   BOOST_CHECK(filters[5].id() == 32001);
 
   std::vector<unsigned int> bl_opts {2u, 2u, 2u, 1048576u, 4u, 0u, 0u}; // ??
   BOOST_REQUIRE_EQUAL(filters[5].cd_values().size(), bl_opts.size());
-  // for(size_t i = 0; i < bl_opts.size(); ++i){
-  //   BOOST_CHECK_EQUAL(filters[5].cd_values()[i], bl_opts[i]);
-  // }
+  for(size_t i = 0; i < bl_opts.size(); ++i){
+    BOOST_CHECK_EQUAL(filters[5].cd_values()[i], bl_opts[i]);
+  }
+  for(auto a: filters[5].cd_values()){
+    std::cout <<"BLo: "<< a << std::endl;
+  }
   BOOST_CHECK(flags[5] == hdf5::filter::Availability::Optional);
+  std::cout << "AF6 " << std::endl;
+
+}
+
+BOOST_AUTO_TEST_CASE(add_filters_mandatory)
+{
+  using hdf5::node::get_node;
+  using hdf5::node::Type;
+  fs::path file = "create/filters_mandatory.xml";
+  std::cout << "MF0" << std::endl;
+  BOOST_CHECK_NO_THROW(xml::create_from_file(root_group,file));
+  std::cout << "MF1" << std::endl;
+
+  BOOST_CHECK(get_node(root_group,"/scan").type() == Type::Group);
+  BOOST_CHECK(get_node(root_group,"/scan/data").type() == Type::Group);
+  BOOST_CHECK(get_node(root_group,"/scan/data/lambda").type() == Type::Dataset);
+  hdf5::node::Dataset dataset = root_group.get_dataset("/scan/data/lambda");
+  std::cout << "MF2" << std::endl;
+  auto dcpl = dataset.creation_list();
+  std::cout << "MF3" << std::endl;
+
+  hdf5::filter::ExternalFilters filters;
+  auto flags = filters.fill(dcpl);
+  std::cout << "MF4: " << dcpl.nfilters() << " " << filters.size() << std::endl;
+  BOOST_CHECK(dcpl.nfilters() == 2);
+  BOOST_CHECK(flags.size() == 2);
+  BOOST_CHECK(filters.size() == 2);
+
+
+  std::cout << "MF5 F0 : " << filters[0].id() << std::endl;
+  BOOST_CHECK(filters[0].id() == 32008);
+
+  std::vector<unsigned int> bs_opts {0u, 3u, 2u, 0u, 2u};    // ??
+  for(auto a: filters[0].cd_values()){
+    std::cout <<"BSm: "<< a << std::endl;
+  }
+  BOOST_REQUIRE_EQUAL(filters[0].cd_values().size(), bs_opts.size());
+  for(size_t i = 1; i < 3; ++i){
+    BOOST_CHECK_EQUAL(filters[0].cd_values()[i], bs_opts[i]);
+  }
+  BOOST_CHECK(flags[0] == hdf5::filter::Availability::Mandatory);
+
+  std::cout << "MF5 F1 : " << filters[1].id() << std::endl;
+  BOOST_CHECK(filters[1].id() == 32001);
+
+  std::vector<unsigned int> bl_opts {2u, 2u, 2u, 1048576u, 4u, 0u, 0u}; // ??
+  BOOST_REQUIRE_EQUAL(filters[1].cd_values().size(), bl_opts.size());
+  for(auto a: filters[1].cd_values()){
+    std::cout <<"BLm: "<< a << std::endl;
+  }
+  for(size_t i = 0; i < bl_opts.size(); ++i){
+    BOOST_CHECK_EQUAL(filters[1].cd_values()[i], bl_opts[i]);
+  }
+  BOOST_CHECK(flags[0] == hdf5::filter::Availability::Mandatory);
+  std::cout << "MF6" << std::endl;
 
 }
 
