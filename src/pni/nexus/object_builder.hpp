f593a22690420df10329af7b4c0389298fcff2ab
Jan Kotanski <jankotan@gmail.com>
2023-10-01 21:27:38 +0200

    add PNINEXUS_EXPORT to ObjectBuilder

--- src/pni/nexus/object_builder.hpp
+++ src/pni/nexus/object_builder.hpp
@@ -33,7 +33,7 @@ namespace nexus {
 //!
 //! This is the basic interface for every Nexus object builder
 //!
-class ObjectBuilder
+class PNINEXUS_EXPORT ObjectBuilder
 {
   public:
     virtual ~ObjectBuilder();
